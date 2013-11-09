2e235b4cf95d072cd331e6294c5fcd5610d96e4e
bmwong042 <brianmkwong28@gmail.com>
2013-11-09 00:52:11 -0500

    updating test
    n

--- Test.py
+++ Test.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
-
-pip install pymongo
+import bottle
+import pymongo
 
 from pymongo import MongoClient
 
 def main():
-    
-    connection = MongoClient()
-    db_overall = connection["db_overall"]
-    col_overall = connection["col_overall"]
-    
-    db_events = connection["event_database"]
-    col_events = db["events"]
-    
-    db_users = connection["user_database"]
-    col_users = db["users"]
-    
-    
+
+	# database for storing users
+
+	db_users = connection["user_database"]
+    db_users.createCollection("col_users")
+
+    # dummy user to set fields
+
+    db_users.col_users.insert(first_name: "first_name", last_name: "last_name" , email: "name@sample.com")
+
+    # -------------------------------------------------------------------
+
+    # database for storing events
+
+	db_events = connection["event_database"]
+	db_events.createCollection("col_events")
+
+	# dummy event to set fields
+
+	db_events.col_events.insert(event_name: "eventname", possible_times: [])
+
+	# -------------------------------------------------------------------
+
+    db = connection["user_database"]
+    db.createCollection("users")
+
+    db.users.insert(first_name: "fname", last_name: "lname" , email: "fl@sample.com")
+
